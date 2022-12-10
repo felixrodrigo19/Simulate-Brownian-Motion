3a49af852faa5b124bc90db13d8976535ba2f3d2
Rodrigo dos Santos Felix <rodrigo.felix@cotefacil.com>
2022-12-10 08:50:14 -0300

    feat: print times array with np.linspace

---
+++ __main__.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    n = 100
+    end_time = 1.
+    times = np.linspace(0., end_time, n)
+    print(f"{times=}")
+
+
+if __name__ == "__main__":
+    main()
